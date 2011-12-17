321abaec2802a69b52325b5d6f4a0b9dffd26f36
'bchruszc' <bradley.chruszcz@gmail.com>
2011-12-16 19:44:25 -0500

    Initial Commit.

---
+++ HelloPhoneGap/src/com/phonegap/android/App.java
@@ -0,0 +1,14 @@
+package com.phonegap.android;
+
+import android.os.Bundle;
+
+import com.phonegap.DroidGap;
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
